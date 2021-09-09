624385c4e1acc0e896b81f9f1de93af6b3846a10
Steven Chen <steven11329@gmail.com>
2021-09-09 11:47:48 +0800

    chore: untrack lib

--- lib/components/TestButton/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="react" />
-import { ButtonProps } from '@material-ui/core/Button';
-declare const TestButton: (props: ButtonProps) => JSX.Element;
-export default TestButton;
--- lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-import Button from './components/Button';
-import TestButton from './components/TestButton';
-import theme from './theme';
-export { Button, TestButton, theme };
