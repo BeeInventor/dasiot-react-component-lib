d6c9c76cb0cc98478693be0c3b82b2cc0514c597
Steven Chen <steven11329@gmail.com>
2023-03-30 17:42:36 +0800

    feat: org text component

---
+++ src/components/OrgText/OrgText.stories.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Meta, Story } from '@storybook/react';
+
+import OrgText, { OrgTextProps } from '.';
+
+export default {
+  title: 'Components/OrgText',
+  component: OrgText,
+  argTypes: {},
+  parameters: {
+    backgrounds: { default: 'light' },
+  },
+} as Meta;
+
+const Template: Story<OrgTextProps> = (args) => <OrgText {...args} />;
+
+export const Default: Story<OrgTextProps> = Template.bind({});
+
+Default.args = {
+  orgName: 'BeeInventor',
+};
+
+export const SpecificColor: Story<OrgTextProps> = Template.bind({});
+
+SpecificColor.args = {
+  ...Default.args,
+  orgColor: '#58B99E',
+};
